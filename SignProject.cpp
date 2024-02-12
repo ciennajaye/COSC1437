4d12306684daccb2869ca089eded5a119ef0eb5a
ciennajaye <140207190+ciennajaye@users.noreply.github.com>
2024-02-12 00:23:21 +0000

    Sign Comments

--- SignProject.cpp
+++ SignProject.cpp
@@ -4,21 +4,26 @@ using namespace std;
 
 int main()
 {
+    //declare variables
     string wood;
     string sign;
     string color;
     int price = 35;
 
+    //ask user for wood type
     cout << "What type of wood would you like for your sign? ";
     cin >> wood;
 
+    //ask user for what do they want the sign to say
     cout << "What do you want your sign to say? ";
     getline(cin, sign);
-
     getline(cin, sign);
+
+    //ask user what color characters they want
     cout << "Would you like black/white characters or golf-leaf lettering? Type (bw) for black or white or type (g) for gold-leaf lettering: ";
     cin >> color;
     
+    //determine the final price
     if (sign.length() > 5)
     {
         price += (sign.length()-5) *4;
@@ -34,6 +39,7 @@ int main()
         price+=15;
     }
 
+    //output final price
     cout << "Your final price is: $" << price << endl;
     
 
