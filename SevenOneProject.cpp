f90eafe60d7b1243162eb7b378015f0f373608fb
ciennajaye <140207190+ciennajaye@users.noreply.github.com>
2024-02-12 00:58:38 +0000

    7.1 Project

---
+++ SevenOneProject.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    //explaining what the program will do
+    cout << "0 through 10 multiplied by 2 and 10" << endl << endl;
+
+    //output
+    for (int i = 0; i <= 10; i++)
+    {
+        cout << "Number: " << i << endl;;
+        cout << "Multiplied by 2: " << (i * 2) << endl;
+        cout << "Multiplied by 10: " << (i *10) << endl;
+        cout << endl;
+    }
+}
